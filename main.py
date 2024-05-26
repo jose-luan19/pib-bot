1201ba207149475ac79a57a0637d862170727677
joseluan620@gmail.com <joseluan620@gmail.com>
2024-05-25 21:08:16 -0300

    init

---
+++ main.py
@@ -0,0 +1,147 @@
+import os
+import time
+import pickle
+import certifi
+import pytz
+from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+
+# Definir a variável de ambiente SSL_CERT_FILE
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+# Definindo o fuso horário do Brasil/Ceará
+CEARA_TZ = pytz.timezone('America/Fortaleza')
+
+# Defina os escopos necessários CLIENT_SECRET
+SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+# Arquivo onde as credenciais serão armazenadas
+youtube = None
+
+def authCredentials():
+    global youtube
+    creds = None
+    token_path = 'token.pickle'
+
+    # Se o token já existir, carregue-o
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
+            creds = pickle.load(token)
+
+    # Se não houver credenciais válidas, faça o login
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            raise Exception("Credenciais inválidas ou expiradas e não podem ser renovadas.")
+
+    # Crie o serviço da API do YouTube
+    youtube = build("youtube", "v3", credentials=creds)
+
+# Horários de verificação em formato 24h
+check_times = ["08:32", "09:02", "17:02", "18:02", "19:32"]
+
+
+# Função para obter a transmissão ao vivo atual
+def get_live_broadcast():
+    authCredentials()
+    request = youtube.liveBroadcasts().list(
+        part="snippet",
+        broadcastStatus="active",
+        broadcastType="all"
+    )
+    response = request.execute()
+
+    if "items" in response and len(response["items"]) > 0:
+        return response["items"][0]
+    else:
+        return None
+
+# Função para enviar uma mensagem no chat ao vivo
+def send_message(live_chat_id, message):
+    request = youtube.liveChatMessages().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "liveChatId": live_chat_id,
+                "type": "textMessageEvent",
+                "textMessageDetails": {
+                    "messageText": message
+                }
+            }
+        }
+    )
+    request.execute()
+
+# Mensagem a ser enviada
+BOAS_VINDAS_DIA = "Bom Dia, sejam bem vindos."
+BOAS_VINDAS_TARDE = "Boa Tarde, sejam bem vindos."
+BOAS_VINDAS_NOITE = "Boa Noite, sejam bem vindos."
+DIVULGACAO_INSTAGRAM = "Acompanhe as atividades e programações da igreja no nosso Instagram - https://www.instagram.com/pibplanaltocaucaia "
+DIVULGACAO_TIKTOK = "E nos acompanhe no TikTok - https://www.tiktok.com/@pibplanaltocaucaia"
+
+
+# Função para calcular o tempo até o próximo horário de verificação
+def time_until_next_check():
+    now = datetime.now(CEARA_TZ)
+    today_checks = [CEARA_TZ.localize(datetime.strptime(time_str, "%H:%M").replace(year=now.year, month=now.month, day=now.day)) for time_str in check_times]
+    future_checks = [check for check in today_checks if check > now]
+    if future_checks:
+        next_check = future_checks[0]
+    else:
+        next_check = today_checks[0] + timedelta(days=1)
+    return (next_check - now).total_seconds() + 60
+
+def prepare_for_send_message_at_time_exact(live_chat_id, message, time_to_wait):
+    time.sleep(time_to_wait)
+    send_message(live_chat_id, message)
+
+def welcome_message(live_chat_id):
+    period = None
+    now = datetime.now(CEARA_TZ)
+    hour_actual = now.hour
+    if 6 <= hour_actual < 12:
+        period = 1
+    elif 12 <= hour_actual < 18:
+        period = 2
+    else:
+        period = 3
+
+    switch = {
+        1: lambda: prepare_for_send_message_at_time_exact(live_chat_id, BOAS_VINDAS_DIA, 5),
+        2: lambda: prepare_for_send_message_at_time_exact(live_chat_id, BOAS_VINDAS_TARDE, 5),
+        3: lambda: prepare_for_send_message_at_time_exact(live_chat_id, BOAS_VINDAS_NOITE, 5)
+    }
+    switch.get(period, lambda: None)()
+
+
+# Função principal para monitorar e enviar mensagens
+def main():
+    broadcast = None
+    try:
+        while True:
+            time_to_sleep = time_until_next_check()
+            print(f"Aguardando {time_to_sleep / 60:.2f} minutos ate a proxima verificaao.")
+            time.sleep(time_to_sleep)
+
+            # Verificar se há uma transmissão ao vivo
+            broadcast = get_live_broadcast()
+            if broadcast:
+                live_chat_id = broadcast["snippet"]["liveChatId"]
+                welcome_message(live_chat_id)
+
+                prepare_for_send_message_at_time_exact(live_chat_id, DIVULGACAO_INSTAGRAM, 150)
+                prepare_for_send_message_at_time_exact(live_chat_id, DIVULGACAO_TIKTOK, 0)
+
+                prepare_for_send_message_at_time_exact(live_chat_id, DIVULGACAO_INSTAGRAM, 4000)
+                prepare_for_send_message_at_time_exact(live_chat_id, DIVULGACAO_TIKTOK, 0)
+            break
+    finally:
+        broadcast = None
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time_to_sleep = 60  
+        time.sleep(time_to_sleep)
